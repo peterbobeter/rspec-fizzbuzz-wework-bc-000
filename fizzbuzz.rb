8e81d9440fc569bd23265c883a380229ee3031e4
Peter Hunt <peter.hunt@wework.com>
2018-02-09 01:01:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,8 +3,10 @@ def fizzbuzz(int)
     "Fizz"
   elsif int % 5 == 0 
     "Buzz" 
-  else int % 5 && 3 == 0 
+  elsif int % 5 == 0 && int % 3 == 0 
     "Fizzbuzz"
+  else int % 5 != 0 || int % 3 != 0 
+    return nil 
   end
 end
 
